aaa379e06696094d3cced95c5829f72cb60336c4
Marvin <manwize@live.com>
2017-03-22 00:57:22 -0400

    Angular Firebase App (Connect to Database)
    Angular Firebase App (Connect to Database)
    Step 2 of 6.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
-
+import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from './services/firebase.service';
+import {Business} from './Business';
+import {Category} from './Category';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
-  title = 'app works!';
-  items: FirebaseListObservable<any[]>;
-  constructor(af: AngularFire) {
-  	this.items = af.database.list('/items');
+export class AppComponent implements OnInit {
+	businesses: Business[];
+	categories: Category[];
+  constructor(private _firebaseService :FirebaseService) {
+  }
+  ngOnInit(){
+  	this._firebaseService.getBusinesses()
+  		.subscribe(businesses => {
+  			this.businesses = businesses;
+		});
+  		this._firebaseService.getCategories()
+  		.subscribe(categories => {
+  			this.categories = categories;
+		});
   }
 }
